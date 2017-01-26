3acd3ade5e297397aab907986aa0cd1893fbe29b
Forbes Lindesay <forbes@lindesay.co.uk>
2017-01-26 00:27:28 +0000

    Update test snapshots

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var assert = require('assert');
 var fs = require('fs');
 var render = require('../');
 
@@ -8,5 +9,16 @@ fs.readdirSync(__dirname + '/examples').filter(function (file) {
 }).forEach(function (file) {
   var str = fs.readFileSync(__dirname + '/examples/' + file, 'utf8');
   var svg = render(str);
-  fs.writeFileSync(__dirname + '/examples/' + file.replace(/\.xml$/, '.svg'), svg);
+  if (process.env.CI) {
+    const expected = fs.readFileSync(__dirname + '/examples/' + file.replace(/\.xml$/, '.svg'));
+    assert.equal(svg, expected);
+  } else {
+    fs.writeFileSync(__dirname + '/examples/' + file.replace(/\.xml$/, '.svg'), svg);
+  }
 });
+
+if (process.env.CI) {
+  console.log('tests passed');
+} else {
+  console.log('tests updated');
+}
